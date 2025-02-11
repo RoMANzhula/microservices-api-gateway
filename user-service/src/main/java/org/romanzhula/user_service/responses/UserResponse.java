2f374a4eb21cdb1920066c47bcc7376164a65336
RoMANzhula <roman1985al@ukr.net>
2025-02-11 22:56:54 +0200

    Add UserResponse - user-service

---
+++ user-service/src/main/java/org/romanzhula/user_service/responses/UserResponse.java
@@ -0,0 +1,17 @@
+package org.romanzhula.user_service.responses;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+
+public class UserResponse {
+
+    // temporary return only id and username (for testing)
+    private Long id;
+    private String username;
+
+//    private String firstName;
+//    private String lastName;
+//    private String email;
+//    private String phoneNumber;
+
+}
