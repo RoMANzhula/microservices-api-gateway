f996ea2889ba3bfdb01b2b8bba5cb3ac31cf972d
RoMANzhula <roman1985al@ukr.net>
2025-02-11 22:55:30 +0200

    Add UserController - user-service - getAll() method

---
+++ user-service/src/main/java/org/romanzhula/user_service/controllers/UserController.java
@@ -0,0 +1,25 @@
+package org.romanzhula.user_service.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.romanzhula.user_service.responses.UserResponse;
+import org.romanzhula.user_service.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/list")
+    public ResponseEntity<List<UserResponse>> getAll() {
+        return userService.getAll();
+    }
+
+}
