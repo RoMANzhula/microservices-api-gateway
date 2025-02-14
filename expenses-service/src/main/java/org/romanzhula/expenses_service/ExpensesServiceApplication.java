746a368df64c030d6de5f7ec0e5c2b3697045e16
RoMANzhula <roman1985al@ukr.net>
2025-02-14 18:08:45 +0200

    Up main classes - all services - fix annotation

--- expenses-service/src/main/java/org/romanzhula/expenses_service/ExpensesServiceApplication.java
+++ expenses-service/src/main/java/org/romanzhula/expenses_service/ExpensesServiceApplication.java
@@ -2,10 +2,10 @@ package org.romanzhula.expenses_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
-@EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ExpensesServiceApplication {
 
