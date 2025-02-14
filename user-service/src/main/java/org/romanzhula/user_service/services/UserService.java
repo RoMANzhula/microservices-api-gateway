1c53c4b33d951ec60357ba5d68b581308b12f584
RoMANzhula <roman1985al@ukr.net>
2025-02-14 18:02:24 +0200

    Up UserService - user-service - add addNewUser() method

--- user-service/src/main/java/org/romanzhula/user_service/services/UserService.java
+++ user-service/src/main/java/org/romanzhula/user_service/services/UserService.java
@@ -1,7 +1,9 @@
 package org.romanzhula.user_service.services;
 
 import lombok.RequiredArgsConstructor;
+import org.romanzhula.user_service.models.User;
 import org.romanzhula.user_service.repositories.UserRepository;
+import org.romanzhula.user_service.requests.UserRequest;
 import org.romanzhula.user_service.responses.UserResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,12 +16,30 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+
     @Transactional(readOnly = true)
     public List<UserResponse> getAll() {
         return userRepository.findAll().stream()
-                .map(user -> new UserResponse())
+                .map(user -> new UserResponse(user.getId(), user.getUsername()))
                 .toList()
         ;
+    }
+
+
+    @Transactional
+    public String addNewUser(UserRequest newUser) {
+        User user = User.builder()
+                .username(newUser.getUsername())
+                .firstName(newUser.getFirstName())
+                .lastName(newUser.getLastName())
+                .email(newUser.getEmail())
+                .phoneNumber(newUser.getPhoneNumber())
+                .build()
+        ;
+
+        userRepository.save(user);
+
+        return "User added successfully!";
     }
 
 }
