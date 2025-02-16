5d70c6c79a91415d9fe861c110a5ec85223720a8
RoMANzhula <roman1985al@ukr.net>
2025-02-16 23:00:13 +0200

    Add Expense - expenses-service

---
+++ expenses-service/src/main/java/org/romanzhula/expenses_service/models/Expense.java
@@ -0,0 +1,34 @@
+package org.romanzhula.expenses_service.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "expenses")
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "expense_id", unique = true, updatable = false, nullable = false)
+    private Long id;
+
+    @Column(name = "user_id", unique = true, updatable = false, nullable = false)
+    private UUID userId;
+
+    @Column(name = "expense_title")
+    private String title;
+
+    @Column(name = "amount", nullable = false, precision = 12, scale = 2)
+    private BigDecimal amount;
+
+    // TODO: add mail sender
+
+}
