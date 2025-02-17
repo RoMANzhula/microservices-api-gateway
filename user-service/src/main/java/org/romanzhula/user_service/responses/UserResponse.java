440194320dedd8b6b5d3d7b12d9925646ecfef0d
RoMANzhula <roman1985al@ukr.net>
2025-02-17 23:18:26 +0200

    Up UserResponse - user-service - change to uuid

--- user-service/src/main/java/org/romanzhula/user_service/responses/UserResponse.java
+++ user-service/src/main/java/org/romanzhula/user_service/responses/UserResponse.java
@@ -3,11 +3,13 @@ package org.romanzhula.user_service.responses;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.UUID;
+
 @Getter
 @AllArgsConstructor
 public class UserResponse {
 
-    private Long id;
+    private UUID id;
     private String username;
     private String firstName;
     private String lastName;
