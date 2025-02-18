6950aff8f6125e349df1862030caad17a2ebdbcc
RoMANzhula <roman1985al@ukr.net>
2025-02-18 23:33:48 +0200

    Add ExpenseService - expenses-service

---
+++ expenses-service/src/main/java/org/romanzhula/expenses_service/services/ExpenseService.java
@@ -0,0 +1,73 @@
+package org.romanzhula.expenses_service.services;
+
+import lombok.RequiredArgsConstructor;
+import org.romanzhula.expenses_service.models.Expense;
+import org.romanzhula.expenses_service.repositories.ExpenseRepository;
+import org.romanzhula.expenses_service.requests.ExpenseRequest;
+import org.romanzhula.expenses_service.responses.ExpenseResponse;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ExpenseService {
+
+    private final WebClient webClient;
+    private final ExpenseRepository expenseRepository;
+
+
+    @Transactional
+    public ExpenseResponse addExpense(ExpenseRequest expenseRequest) {
+        String userId = expenseRequest.getUserId().toString();
+        String walletServiceGetBalanceUrl = "http://localhost:8082/api/v1/wallets/" + userId + "/balance";
+        String walletServiceUpdateBalanceUrl = "http://localhost:8082/api/v1/wallets/" + userId + "/deduct-balance";
+
+        BigDecimal currentBalance = webClient
+                .get()
+                .uri(walletServiceGetBalanceUrl)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<Map<String, BigDecimal>>() {})
+                .map(response -> response.get("balance"))
+                .block();
+
+        if (currentBalance == null) {
+            throw new RuntimeException("Failed to retrieve user balance");
+        }
+
+        BigDecimal expenseAmount = expenseRequest.getAmount();
+
+        if (currentBalance.compareTo(expenseAmount) < 0) {
+            throw new IllegalArgumentException("Insufficient funds for the expense");
+        }
+
+        Expense expense = new Expense();
+        expense.setUserId(UUID.fromString(expenseRequest.getUserId()));
+        expense.setTitle(expenseRequest.getTitle());
+        expense.setAmount(expenseAmount);
+
+        Expense savedExpense = expenseRepository.save(expense);
+
+        // TODO: add RabbitMQ convertAndSend here to change balance (wallet-service)
+
+        return new ExpenseResponse(
+                savedExpense.getId(),
+                savedExpense.getUserId(),
+                savedExpense.getTitle(),
+                savedExpense.getAmount()
+        );
+    }
+
+
+    @Transactional(readOnly = true)
+    public List<ExpenseResponse> getAllExpensesByUserId(String userId) {
+        return expenseRepository.findAllByUserId(UUID.fromString(userId));
+    }
+
+}
