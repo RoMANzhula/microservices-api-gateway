58382909089365468fdb273b1bdab2f5227686f3
RoMANzhula <roman1985al@ukr.net>
2025-02-21 16:16:27 +0200

    Add JournalEntryResponse - journal-service

---
+++ journal-service/src/main/java/org/romanzhula/journal_service/responses/JournalEntryResponse.java
@@ -0,0 +1,14 @@
+package org.romanzhula.journal_service.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class JournalEntryResponse {
+
+    private String message;
+
+}
