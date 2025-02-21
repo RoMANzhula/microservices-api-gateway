67f78dd0710e38a8e3baf2d027d635c3ecc8be37
RoMANzhula <roman1985al@ukr.net>
2025-02-21 22:33:43 +0200

    Add JournalEntryResponse - wallet-service

---
+++ wallet-service/src/main/java/org/romanzhula/wallet_service/responses/JournalEntryResponse.java
@@ -0,0 +1,12 @@
+package org.romanzhula.wallet_service.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class JournalEntryResponse {
+
+    private String message;
+
+}
