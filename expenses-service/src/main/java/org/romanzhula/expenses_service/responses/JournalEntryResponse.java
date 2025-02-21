617535ec2aea2b3ce9622813bf6952df87637b89
RoMANzhula <roman1985al@ukr.net>
2025-02-21 16:38:03 +0200

    Add JournalEntryResponse - expenses-service - rest communication with journal-service

---
+++ expenses-service/src/main/java/org/romanzhula/expenses_service/responses/JournalEntryResponse.java
@@ -0,0 +1,12 @@
+package org.romanzhula.expenses_service.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class JournalEntryResponse {
+
+    private String message;
+
+}
