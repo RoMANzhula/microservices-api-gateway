d93754cdac23a2413d0dbab5a79f1a35ab8e0e9b
RoMANzhula <roman1985al@ukr.net>
2025-02-22 23:19:37 +0200

    Up WalletService - wallet-service - optimize code

--- wallet-service/src/main/java/org/romanzhula/wallet_service/services/WalletService.java
+++ wallet-service/src/main/java/org/romanzhula/wallet_service/services/WalletService.java
@@ -9,6 +9,7 @@ import org.romanzhula.wallet_service.requests.JournalEntryRequest;
 import org.romanzhula.wallet_service.responses.JournalEntryResponse;
 import org.romanzhula.wallet_service.responses.WalletBalanceResponse;
 import org.romanzhula.wallet_service.responses.WalletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,53 +18,107 @@ import org.springframework.web.reactive.function.client.WebClient;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class WalletService {
 
+    @Value("${journal.service.addEntryUrl}")
+    private String journalEntryRequestUrl;
+
     private final WalletRepository walletRepository;
     private final WebClient webClient;
 
-    
+
     @Transactional(readOnly = true)
     public WalletResponse getWalletById(UUID id) {
         return walletRepository.findById(id)
-                .map(wallet -> new WalletResponse(
-                        wallet.getUserId(),
-                        wallet.getBalance()
-                ))
+                .map(wallet -> new WalletResponse(wallet.getUserId(), wallet.getBalance()))
                 .orElseThrow(() -> new EntityNotFoundException("Wallet not found with id: " + id))
         ;
     }
 
+
     @Transactional
     public String updateBalance(BalanceUpdateRequest request) {
-        String journalEntryRequestUrl = "http://localhost:8083/api/v1/journal/add";
+        validateBalanceUpdateRequest(request);
 
+        Wallet wallet = fetchWalletById(request.getId());
+        updateWalletBalance(wallet, request.getAmount());
+
+        JournalEntryResponse journalResponse = createJournalEntry(request);
+        validateJournalEntryResponse(journalResponse);
+
+        return "Your balance was updated successfully!";
+    }
+
+
+    @Transactional(readOnly = true)
+    public List<WalletResponse> getAll() {
+        return walletRepository.findAll()
+                .stream()
+                .map(wallet -> new WalletResponse(wallet.getUserId(), wallet.getBalance()))
+                .collect(Collectors.toList())
+        ;
+    }
+
+
+    @Transactional(readOnly = true)
+    public WalletBalanceResponse getWalletBalanceById(UUID id) {
+        return walletRepository.findById(id)
+                .map(wallet -> new WalletBalanceResponse(wallet.getBalance()))
+                .orElseThrow(() -> new EntityNotFoundException("Wallet not found with id: " + id))
+        ;
+    }
+
+
+    @Transactional
+    public String deductBalance(BalanceUpdateRequest request) {
+        validateDeductionRequest(request);
+
+        Wallet wallet = fetchWalletById(request.getId());
+        checkSufficientFunds(wallet, request.getAmount());
+
+        wallet.setBalance(wallet.getBalance().subtract(request.getAmount()));
+        walletRepository.save(wallet);
+
+        return "Your balance successfully deducted!";
+    }
+
+
+    private void validateBalanceUpdateRequest(BalanceUpdateRequest request) {
         if (request.getAmount().compareTo(BigDecimal.ZERO) <= 0) {
             throw new IllegalArgumentException("The top-up amount must be greater than 0.");
         }
+    }
 
-        Wallet wallet = walletRepository
-                .findById(UUID.fromString(request.getId()))
-                .orElseThrow(() -> new EntityNotFoundException("Wallet not found with id: " + request.getId()));
 
+    private Wallet fetchWalletById(String walletId) {
+        return walletRepository.findById(UUID.fromString(walletId))
+                .orElseThrow(() -> new EntityNotFoundException("Wallet not found with id: " + walletId));
+    }
+
+
+    private void updateWalletBalance(Wallet wallet, BigDecimal amount) {
         if (wallet == null) {
-            handleUserCreated(request.getId());
+            handleUserCreated(wallet.getUserId().toString());
         } else {
-            wallet.setBalance(wallet.getBalance().add(request.getAmount()));
+            wallet.setBalance(wallet.getBalance().add(amount));
         }
 
         walletRepository.save(wallet);
+    }
+
+
+    private JournalEntryResponse createJournalEntry(BalanceUpdateRequest request) {
 
         JournalEntryRequest journalEntryRequest = new JournalEntryRequest(
                 UUID.fromString(request.getId()),
                 "Your balance was updated successfully! +" + request.getAmount()
         );
 
-        JournalEntryResponse successResponse = webClient
-                .post()
+        return webClient.post()
                 .uri(journalEntryRequestUrl)
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(journalEntryRequest)
@@ -71,48 +126,17 @@ public class WalletService {
                 .bodyToMono(JournalEntryResponse.class)
                 .block()
         ;
+    }
 
-        if (successResponse == null || !"New journal entry was added successfully.".equals(successResponse.getMessage())) {
+
+    private void validateJournalEntryResponse(JournalEntryResponse journalResponse) {
+        if (journalResponse == null || !"New journal entry was added successfully.".equals(journalResponse.getMessage())) {
             throw new RuntimeException("Failed to update wallet balance FOR JOURNAL_SERVICE");
         }
-
-        return "Your balance was updated successfully!";
     }
 
-    // TODO: change to new method with @RabbitListener
-    // temporary method
-    public void handleUserCreated(String id) {
-        Wallet wallet = new Wallet();
-        wallet.setUserId(UUID.randomUUID());
-        wallet.setBalance(BigDecimal.valueOf(0.0));
 
-        walletRepository.save(wallet);
-    }
-
-    // TODO: add pagination or delete this logic for all elements
-    @Transactional(readOnly = true)
-    public List<WalletResponse> getAll() {
-        return walletRepository.findAll().stream()
-                .map(wallet -> new WalletResponse(
-                        wallet.getUserId(),
-                        wallet.getBalance()
-                ))
-                .toList()
-        ;
-    }
-
-    @Transactional(readOnly = true)
-    public WalletBalanceResponse getWalletBalanceById(UUID id) {
-        return walletRepository.findById(id)
-                .map(wallet -> new WalletBalanceResponse(
-                        wallet.getBalance()
-                ))
-                .orElseThrow(() -> new EntityNotFoundException("Wallet not found with id: " + id))
-        ;
-    }
-
-    @Transactional
-    public String deductBalance(BalanceUpdateRequest request) {
+    private void validateDeductionRequest(BalanceUpdateRequest request) {
         if (request.getAmount().compareTo(BigDecimal.ZERO) <= 0) {
             throw new IllegalArgumentException("The deduction amount must be greater than 0.");
         }
@@ -120,22 +144,21 @@ public class WalletService {
         if (request.getId() == null || request.getId().isEmpty()) {
             throw new IllegalArgumentException("Wallet ID cannot be null or empty.");
         }
+    }
 
-        UUID walletId = UUID.fromString(request.getId());
 
-        Wallet wallet = walletRepository
-                .findById(walletId)
-                .orElseThrow(() -> new EntityNotFoundException("Wallet not found with id:" + request.getId()))
-        ;
-
-        if (wallet.getBalance().compareTo(request.getAmount()) < 0) {
+    private void checkSufficientFunds(Wallet wallet, BigDecimal deductionAmount) {
+        if (wallet.getBalance().compareTo(deductionAmount) < 0) {
             throw new IllegalArgumentException("Insufficient funds in the wallet.");
         }
+    }
 
-        wallet.setBalance(wallet.getBalance().subtract(request.getAmount()));
+
+    public void handleUserCreated(String id) {
+        Wallet wallet = new Wallet();
+        wallet.setUserId(UUID.randomUUID());
+        wallet.setBalance(BigDecimal.valueOf(0.0));
         walletRepository.save(wallet);
-
-        return "Your balance successfully deducted!";
     }
 
 }
